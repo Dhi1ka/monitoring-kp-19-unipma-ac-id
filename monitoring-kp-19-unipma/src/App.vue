88539de942f6b2696c7cf201aa41a3666fa637d7
dhi1ka <daw.knight04@gmail.com>
2022-02-15 08:48:48 +0700

    router

--- monitoring-kp-19-unipma/src/App.vue
+++ monitoring-kp-19-unipma/src/App.vue
@@ -1,24 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <Navbar />
+    <router-view></router-view>
+  </div>
 </template>
 
 <style>
-@import './assets/base.css';
+@import "./assets/base.css";
 
 #app {
   max-width: 1280px;
