03af96ba69bffcbae72716ea1c701e4c8fba4a30
dhi1ka <daw.knight04@gmail.com>
2022-02-19 00:09:40 +0700

    tailwind

--- monitoring-kp-19-unipma-admin/src/App.vue
+++ monitoring-kp-19-unipma-admin/src/App.vue
@@ -1,18 +1,37 @@
-<script>
-import Navbar from "./components/Navbar.vue";
-export default {
-  name: "App",
-  components: {
-    Navbar,
-  },
-};
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import menu from '@/menu.js'
+import NavBar from '@/components/NavBar.vue'
+import AsideMenu from '@/components/AsideMenu.vue'
+import FooterBar from '@/components/FooterBar.vue'
+import Overlay from '@/components/Overlay.vue'
+
+const store = useStore()
+
+store.commit('user', {
+  name: 'John Doe',
+  email: 'john@example.com',
+  avatar: 'https://avatars.dicebear.com/api/avataaars/example.svg?options[top][]=shortHair&options[accessoriesChance]=93'
+})
+
+const isAsideLgActive = computed(() => store.state.isAsideLgActive)
+
+const overlayClick = () => {
+  store.dispatch('asideLgToggle', false)
+}
 </script>
 
 <template>
   <div>
-    <Navbar />
-    <router-view></router-view>
+    <nav-bar />
+    <aside-menu :menu="menu" />
+    <router-view />
+    <footer-bar />
+    <overlay
+      v-show="isAsideLgActive"
+      z-index="z-30"
+      @overlay-click="overlayClick"
+    />
   </div>
 </template>
-
-<style></style>
