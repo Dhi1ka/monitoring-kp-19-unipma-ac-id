0a2f21bfc3eaf5d7ff4edca25c63287c7387087d
dhi1ka <daw.knight04@gmail.com>
2022-02-18 08:10:52 +0700

    admin

---
+++ monitoring-kp-19-unipma-admin/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<style></style>
---
+++ monitoring-kp-19-unipma-admin/src/views/Home.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+  </div>
+</template>
+
+<style></style>
