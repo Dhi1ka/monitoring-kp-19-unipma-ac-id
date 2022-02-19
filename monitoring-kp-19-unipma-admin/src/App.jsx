3d01243e40a611d4e95f3f617bd9cdcca03ceead
dhi1ka <daw.knight04@gmail.com>
2022-02-19 23:07:53 +0700

    components

--- monitoring-kp-19-unipma-admin/src/App.jsx
+++ monitoring-kp-19-unipma-admin/src/App.jsx
@@ -1,45 +1,26 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Header, Sidebar } from "./components";
+import { Dashboard, Students } from "./views";
+import "./assets/dashboard.css";
+import "./assets/bootstrap.min.css";
+import "./assets/main.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <>
+      <Header />
+      <div className="container-fluid">
+        <div className="row">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Dashboard />}></Route>
+            <Route path="/students" element={<Students />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
